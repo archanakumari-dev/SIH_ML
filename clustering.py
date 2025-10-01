ff4828e1a8432a22df2909d6ba4e2e3f4a2c483a
Ayush jha <ayu.jha321@gmail.com>
2025-10-01 12:12:04 +0530

    final commit

--- clustering.py
+++
@@ -1,4 +0,0 @@
-from sklearn.cluster import DBSCAN
-def run_dbscan(X):
-    db = DBSCAN(eps=0.3, min_samples=5, metric='cosine')
-    return db.fit_predict(X)
--- label_encoding.py
+++
@@ -1,6 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-
-def encode_species(df):
-    le = LabelEncoder()
-    df['species_encoded'] = le.fit_transform(df['species'])
-    return df, le
